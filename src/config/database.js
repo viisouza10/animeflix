c551af04dae7ff1750572df46706865f82e695c7
Vinicius Souza <viisouza10@live.com>
2020-03-10 22:07:32 -0300

    config db

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'animeflix',
-  port:5432,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   logging: false,
   define: {
     timestamps: true,
